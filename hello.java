94bb1ce391604084e9749bfc9c7694fca988258c
Rahul <rahulkhannaexe@gmail.com>
2023-12-28 14:24:45 +0530

    another Commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+class hello
+{
+    public static void main(String args[])
+    {
+        int i=5;
+        do
+       {
+            System.out.println("hi " + i);
+            i++;
+        } while(i<=4);
+    }
+}
